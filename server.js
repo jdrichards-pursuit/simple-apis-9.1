34cf2126e14b7f0e6bc00bafae852846d074346b
jd richards <jdrichardstech@gmail.com>
2022-07-08 15:29:45 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const cors = require("cors");
-const jsonServer = require("json-server");
-const path = require("path");
-const { validateResource } = require("./util");
+const cors = require('cors');
+const jsonServer = require('json-server');
+const path = require('path');
+const { validateResource } = require('./util');
 
 const app = jsonServer.create();
-const dbPath = path.join(__dirname, "data", "db.json");
+const dbPath = path.join(__dirname, 'data', 'db.json');
 const router = jsonServer.router(dbPath);
 const middleware = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Enable all CORS requests.
 app.use(cors());
 
 // Index route message.
-app.get("/", (_req, _res, next) => {
+app.get('/', (_req, _res, next) => {
   next({
-    message: "No resource found.",
-    status: 404,
+    message: 'No resource found.',
+    status: 404
   });
 });
 
@@ -25,10 +25,10 @@ app.use(middleware);
 
 // Only allow GET requests.
 app.use((req, _res, next) => {
-  if (req.method !== "GET") {
+  if (req.method !== 'GET') {
     return next({
       message: `${req.method} ${req.path} is not allowed.`,
-      status: 405,
+      status: 405
     });
   }
 
@@ -36,19 +36,19 @@ app.use((req, _res, next) => {
 });
 
 // Mount routes.
-app.use("/api", validateResource, router);
+app.use('/api', validateResource, router);
 
 // Catch-all handler.
 app.use((req, _res, next) => {
   next({
     message: `${req.method} ${req.path} is not found.`,
-    status: 404,
+    status: 404
   });
 });
 
 // Error handler.
 app.use(
-  ({ status = 500, message = "Internal server error." }, _req, res, _next) => {
+  ({ status = 500, message = 'Internal server error.' }, _req, res, _next) => {
     res.status(status).json({ error: message });
   }
 );
